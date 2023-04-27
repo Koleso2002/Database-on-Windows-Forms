8fdc7447d68af01e5b6c905274fab35ee4452a0c
Konstantin Kolesnikov <kol42@mail.ru>
2023-04-27 16:08:16 +0700

    Внёс изменения в код Form3

--- WindowsFormsApp1/Form3.cs
+++ WindowsFormsApp1/Form3.cs
@@ -9,11 +9,16 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.Controllers;
+using WindowsFormsApp1.Models;
 
 namespace WindowsFormsApp1
 {
     public partial class Form3 : Form
     {
+        private ModelPreventivRepairs preventivRepairs;
+        private AddPrevetiveRepairs addPrevetiveRepairs;
+
         private static SqlConnection conn;
         private static string path = @"Data Source=KONSTANTIN\SQLEXPRESS;
                             Initial Catalog=Equipment;
@@ -25,55 +30,81 @@ namespace WindowsFormsApp1
         public Form3()
         {
             InitializeComponent();
+            preventivRepairs=new ModelPreventivRepairs();
         }
+
+
+
 
         private void AddRepairs()
         {
-            string eqp;
-            DateTime date;
-            double price;
-            string operations;
-
             bool flag = true;
             while (flag)
             {
                 try
                 {
-                    eqp = equip.Where(x => x.Value == textBox1.Text).FirstOrDefault().Key.ToString();
-                    date = dateTimePicker1.Value;
-                    price = Double.Parse(textBox2.Text);
-                    operations = textBox3.Text;
+                    preventivRepairs.NameEquipment = textBox1.Text;
+                    preventivRepairs.date = dateTimePicker1.Value;
+                    preventivRepairs.price = Double.Parse(textBox2.Text);
+                    preventivRepairs.operation = textBox3.Text;
                     flag = false;
-
                 }
+
                 catch (Exception ex)
                 {
                     MessageBox.Show("В поле \"Сумма\" неверный формат. Попробуйте ввести копейки через запятую!");
                 }
-
-                using (conn = new SqlConnection(path))
-                {
-                    string command = @"select * from PreventiveRepairs;";
-                    SqlDataAdapter adapter = new SqlDataAdapter(command, conn);
-                    DataSet ds = new DataSet();
-                    adapter.Fill(ds);
-                    DataTable tb = ds.Tables[0];
-                    DataRow newRow = tb.NewRow();
-
-                    newRow[1] = equip.Where(x => x.Value == textBox1.Text).FirstOrDefault().Key;
-                    newRow[2] = dateTimePicker1.Value;
-                    newRow[3] = Double.Parse(textBox2.Text);
-                    newRow[4] = textBox3.Text;
-
-                tb.Rows.Add(newRow);
-                SqlCommandBuilder sqlCommand = new SqlCommandBuilder(adapter);
-                adapter.Update(tb);
-                }
             }
+
+
+            //  string eqp;
+            //DateTime date;
+            //double price;
+            //string operations;
+
+            //bool flag = true;
+            //while (flag)
+            //{
+            //    try
+            //    {
+            //        eqp = equip.Where(x => x.Value == textBox1.Text).FirstOrDefault().Key.ToString();
+            //        date = dateTimePicker1.Value;
+            //        price = Double.Parse(textBox2.Text);
+            //        operations = textBox3.Text;
+            //        flag = false;
+
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        MessageBox.Show("В поле \"Сумма\" неверный формат. Попробуйте ввести копейки через запятую!");
+            //    }
+
+            //    using (conn = new SqlConnection(path))
+            //    {
+            //        string command = @"select * from PreventiveRepairs;";
+            //        SqlDataAdapter adapter = new SqlDataAdapter(command, conn);
+            //        DataSet ds = new DataSet();
+            //        adapter.Fill(ds);
+            //        DataTable tb = ds.Tables[0];
+            //        DataRow newRow = tb.NewRow();
+
+            //        newRow[1] = equip.Where(x => x.Value == textBox1.Text).FirstOrDefault().Key;
+            //        newRow[2] = dateTimePicker1.Value;
+            //        newRow[3] = Double.Parse(textBox2.Text);
+            //        newRow[4] = textBox3.Text;
+
+            //    tb.Rows.Add(newRow);
+            //    SqlCommandBuilder sqlCommand = new SqlCommandBuilder(adapter);
+            //    adapter.Update(tb);
+            //    }
+            //}
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            addPrevetiveRepairs = new AddPrevetiveRepairs(preventivRepairs);
+            
+            
             AddRepairs();
             this.Close();
         }
