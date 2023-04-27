336b5c31a1973a3d720d96731b3a0e16230e620b
Konstantin Kolesnikov <kol42@mail.ru>
2023-04-27 17:04:27 +0700

    Добавил проверку правильности заполнения полей

--- WindowsFormsApp1/Form3.cs
+++ WindowsFormsApp1/Form3.cs
@@ -30,83 +30,88 @@ namespace WindowsFormsApp1
         public Form3()
         {
             InitializeComponent();
-            preventivRepairs=new ModelPreventivRepairs();
+            preventivRepairs = new ModelPreventivRepairs();
+            textBox2.LostFocus += TextBox2_LostFocus;
         }
 
-
-
-
-        private void AddRepairs()
+        private void TextBox2_LostFocus(object sender, EventArgs e)
         {
-            bool flag = true;
-            while (flag)
+            if (Double.TryParse(textBox2.Text, out var pr))
             {
-                try
-                {
-                    preventivRepairs.NameEquipment = textBox1.Text;
-                    preventivRepairs.date = dateTimePicker1.Value;
-                    preventivRepairs.price = Double.Parse(textBox2.Text);
-                    preventivRepairs.operation = textBox3.Text;
-                    flag = false;
-                }
-
-                catch (Exception ex)
-                {
-                    MessageBox.Show("В поле \"Сумма\" неверный формат. Попробуйте ввести копейки через запятую!");
-                }
+                preventivRepairs.price = pr;
             }
-
-
-            //  string eqp;
-            //DateTime date;
-            //double price;
-            //string operations;
-
-            //bool flag = true;
-            //while (flag)
-            //{
-            //    try
-            //    {
-            //        eqp = equip.Where(x => x.Value == textBox1.Text).FirstOrDefault().Key.ToString();
-            //        date = dateTimePicker1.Value;
-            //        price = Double.Parse(textBox2.Text);
-            //        operations = textBox3.Text;
-            //        flag = false;
-
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        MessageBox.Show("В поле \"Сумма\" неверный формат. Попробуйте ввести копейки через запятую!");
-            //    }
-
-            //    using (conn = new SqlConnection(path))
-            //    {
-            //        string command = @"select * from PreventiveRepairs;";
-            //        SqlDataAdapter adapter = new SqlDataAdapter(command, conn);
-            //        DataSet ds = new DataSet();
-            //        adapter.Fill(ds);
-            //        DataTable tb = ds.Tables[0];
-            //        DataRow newRow = tb.NewRow();
-
-            //        newRow[1] = equip.Where(x => x.Value == textBox1.Text).FirstOrDefault().Key;
-            //        newRow[2] = dateTimePicker1.Value;
-            //        newRow[3] = Double.Parse(textBox2.Text);
-            //        newRow[4] = textBox3.Text;
-
-            //    tb.Rows.Add(newRow);
-            //    SqlCommandBuilder sqlCommand = new SqlCommandBuilder(adapter);
-            //    adapter.Update(tb);
-            //    }
-            //}
+            else
+            {
+                MessageBox.Show("В поле \"Сумма\" неверный формат данных. Попробуйте ввести копейки через запятую!");
+                textBox2.Focus();
+            }
         }
+
+        private bool AddRepairs()
+        {
+            bool flag = false;
+            preventivRepairs.NameEquipment = textBox1.Text;
+            preventivRepairs.date = dateTimePicker1.Value;
+            if (textBox3.Text != string.Empty)
+            {
+                preventivRepairs.operation = textBox3.Text;
+                flag = true;
+            }
+            return flag;
+        }
+
+
+        //  string eqp;
+        //DateTime date;
+        //double price;
+        //string operations;
+
+        //bool flag = true;
+        //while (flag)
+        //{
+        //    try
+        //    {
+        //        eqp = equip.Where(x => x.Value == textBox1.Text).FirstOrDefault().Key.ToString();
+        //        date = dateTimePicker1.Value;
+        //        price = Double.Parse(textBox2.Text);
+        //        operations = textBox3.Text;
+        //        flag = false;
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show("В поле \"Сумма\" неверный формат. Попробуйте ввести копейки через запятую!");
+        //    }
+
+        //    using (conn = new SqlConnection(path))
+        //    {
+        //        string command = @"select * from PreventiveRepairs;";
+        //        SqlDataAdapter adapter = new SqlDataAdapter(command, conn);
+        //        DataSet ds = new DataSet();
+        //        adapter.Fill(ds);
+        //        DataTable tb = ds.Tables[0];
+        //        DataRow newRow = tb.NewRow();
+
+        //        newRow[1] = equip.Where(x => x.Value == textBox1.Text).FirstOrDefault().Key;
+        //        newRow[2] = dateTimePicker1.Value;
+        //        newRow[3] = Double.Parse(textBox2.Text);
+        //        newRow[4] = textBox3.Text;
+
+        //    tb.Rows.Add(newRow);
+        //    SqlCommandBuilder sqlCommand = new SqlCommandBuilder(adapter);
+        //    adapter.Update(tb);
+        //    }
+        //}
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            addPrevetiveRepairs = new AddPrevetiveRepairs(preventivRepairs);
-            
-            
-            AddRepairs();
-            this.Close();
+
+            if (AddRepairs())
+            {
+                addPrevetiveRepairs = new AddPrevetiveRepairs(preventivRepairs);
+                this.Close();
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
